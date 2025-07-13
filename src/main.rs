85858491c8358f79e9b0602fcfa3e9884246ba98
Manu Farfaro <manu.farfaro@gmail.com>
2025-07-13 00:21:51 -0300

    feat: initial bank structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+fn print_account(account: &Account) {
+    println!("Account: {account:#?}");
+}
+
 fn main() {
-    println!("Hello, Rust!");
+    let bank = Bank::new();
+
+    let account = Account::new(1, String::from("Manu"));
+
+    println!("Bank: {bank:#?}");
+
+    print_account(&account);
 }
